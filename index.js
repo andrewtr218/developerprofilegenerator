4c64075d4f2f9c80bf66462e775adda4227ad8f9
andrewtr218 <andrewtr218@gmail.com>
2020-01-12 17:20:03 -0600

    Styling updated, google maps repurposed to use the target github users location rather than the application users location, having issues implementing conversion to PDF

--- index.js
+++ index.js
@@ -72,7 +72,13 @@ function dataPick(data, data2, q2){
         </div>
         <div>
             <p id='userName'>Username: ${data.data.login}</p>
-            <p id='map'></p>
+            <div id='map'><iframe
+            width="420"
+            height="420"
+            frameborder="0" style="border:0"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDFf91xJ4O4KtnZNK-bos0QnKt4V-VrqUk
+              &q=${data.data.location}" allowfullscreen>
+          </iframe></div>
             <p id='githubProf'>GitHub: <a href="${data.data.html_url}">${data.data.html_url}</a></p>
             <p id='userBlog'>Blog: <a href="${data.data.blog}">${data.data.blog}</a></p>
             <p id='userBio'>Bio: ${data.data.bio}</p>
@@ -81,58 +87,24 @@ function dataPick(data, data2, q2){
             <p id="gitStars">Number of Stars: ${data2.data[0].stargazers_count}</p>
             <p id='numUsersFollow'>Following: ${data.data.following}</p>
         </div>
-        <script>
-      // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
-      var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              "Error: The Geolocation service failed." :
-                              "Error: Your browser doesn\'t support geolocation.");
-        infoWindow.open(map);
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDFf91xJ4O4KtnZNK-bos0QnKt4V-VrqUk&callback=initMap">
-    </script>
     </body>
     </html>`
         fs.writeFile("index.html", $html, function(err){
             if (err) throw err;
             console.log("Error in writefile")
         })
+        //     conversion({ html: $html }, (err, result) => {
+        //         if (err) return console.log("Error in conversion");
+        //         result.stream.pipe(fs.createWriteStream('./devProfile.pdf'));
+        //         conversion.kill();
+            
+        // })
 }
+
+// const conversion = convertFactory({
+//     converterPath: convertFactory.converters.PDF,
+//     allowLocalFilesAccess: true
+//   });
 
 function start (){
     inquirer.prompt(questions).then(( { q1 , q2 } ) => {
